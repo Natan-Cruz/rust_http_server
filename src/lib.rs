c2dee88629e5598e12a2b887f4178fc55c192e22
Natã Alves <natanalves287@gmail.com>
2024-01-13 15:13:07 -0300

    make public the lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,28 @@
-use serde::{Serialize, Deserialize};
 
 mod http_server;
 mod http_response;
 mod http_request;
 mod http_input_stream;
 
-#[derive(Serialize, Deserialize)]
-struct User<'a> {
-    id: &'a str,
-    name: &'a str
-}
+pub use self::http_server::{HttpServer, Route};
+
 
 #[cfg(test)]
 pub mod tests {
-    use crate::User;
     use crate::http_server;
     use crate::http_response;
+    use serde::{Serialize, Deserialize};
+
+    #[derive(Serialize, Deserialize)]
+    struct User<'a> {
+        id: &'a str,
+        name: &'a str
+    }
 
     #[test]
     fn test(){
         let mut server = http_server::HttpServer::new();
+        
 
         server.get("/users", Box::new(|_| {
             let mut response = http_response::HttpResponse::new();
